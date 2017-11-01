9a8449919b012e1feca8d73783bb5bf0fec269df
Vitor Andrade <vgabrielteixeira@gmail.com>
2017-11-01 15:53:42 -0200

    Assossiative Array

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello WOrld!";
+$task = [
+	'title'	=> "Estudar PhP",
+	'due'	=>	"Aprender php pra beck-end",
+	'assigned_to' => "eu",
+	'completed' => false
+];
+
+var_dump($task);
+
+?>
