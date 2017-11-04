61ae72da08c4d7f00bb5d242711b3fb4134d57a5
Vitor Andrade <vgabrielteixeira@gmail.com>
2017-11-04 00:16:42 -0200

    Aula 19 completa POST/GET

--- core/Router.php
+++ core/Router.php
@@ -1,20 +1,29 @@
 <?php
 
 class Router{
-	protected $router = [];
+	public $router = [
+		'GET' => [],
+		'POST' =>[]
+	];
 
 	public static function load($file){
 		$router = new static;
 		require $file;
 		return $router;
 	}
+	public function get($uri, $controller){
+		$this->router['GET'][$uri] = $controller;
+	}
 
+	public function post($uri, $controller){
+		$this->router['POST'][$uri] = $controller;
+	}
 	public function define($routes){
 		$this->router = $routes;
 	}
 
-	public function direct($uri){
-		if(array_key_exists($uri, $this->router)){
+	public function direct($uri,$requestType){
+		if(array_key_exists($uri, $this->router[$requestType])){
 			return $this->router[$uri];
 		}else{
 			throw new Exception("No route defined for this URI $uri");
--- routes.php
+++ routes.php
@@ -4,4 +4,12 @@ $router->define([
 	"" => 'controllers\index.php',
 	'contact' => 'controllers\contact.php',
 	'about' => 'controllers\about.php',
-	'about/culture' => 'controllers\about-culture.php']);
+	'about/culture' => 'controllers\about-culture.php',
+	'names' => 'controllers/add-name.php'
+]);
+
+$router->get('','controllers/index.php');
+$router->get('contact','controllers/contact.php');
+$router->get('about','controllers/about.php');
+$router->get('about/culture','controllers/about-culture.php');
+$router->post('names','controllers/add-name.php');
